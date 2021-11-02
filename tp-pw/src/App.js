313ac9163e7a2774297bb62f904f1cd4e33184b9
Florencia <lombardo.mflorencia@gmail.com>
2021-11-02 10:50:08 -0300

    React funcionando (con Bootstrap?)

--- tp-pw/src/App.js
+++ tp-pw/src/App.js
@@ -2,24 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+ const products = ["Kit S de Girgolas", "Kit M de Girgolas", "Kit L de Girgolas", "Kit..."]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+	   
+     <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Tienda</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Mi perfil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contactanos</a>
+      </li>
+     </ul>
+
+       <div>
+         
+
+       </div>
+	
     </div>
+    
+    
   );
+
+  
 }
 
 export default App;
