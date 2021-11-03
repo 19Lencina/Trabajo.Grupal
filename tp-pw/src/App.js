08c66c4c933b8ce605926dea375aef033066033f
Florencia <lombardo.mflorencia@gmail.com>
2021-11-03 10:04:48 -0300

    Cards

--- tp-pw/src/App.js
+++ tp-pw/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
 
- const products = ["Kit S de Girgolas", "Kit M de Girgolas", "Kit L de Girgolas", "Kit..."]
+ const products = ['Kit S de Girgolas', 'Kit M de Girgolas', 'Kit L de Girgolas']
 
   return (
     <div>
@@ -25,7 +25,29 @@ function App() {
      </ul>
 
        <div>
-         
+         {
+           products.map((product) => {
+             return(
+                <div className="card">
+                  <img src="..." className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">{product}</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item">An item</li>
+                    <li className="list-group-item">A second item</li>
+                    <li className="list-group-item">A third item</li>
+                  </ul>
+                  <div className="card-body">
+                    <a href="#" className="card-link">Card link</a>
+                    <a href="#" className="card-link">Another link</a>
+                  </div>
+                </div>
+
+              )
+           })
+         }
 
        </div>
 	
